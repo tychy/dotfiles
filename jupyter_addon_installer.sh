b8427bfc9dbf578181c7e548fbbe2cf20bcf9fb4
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2020-09-16 19:10:38 +0900

    add : jupyter addon installer

---
+++ jupyter_addon_installer.sh
@@ -0,0 +1,18 @@
+pip install jupyter jupyterlab autopep8
+#jupyter
+jupyter labextension install @lckr/jupyterlab_variableinspector
+
+pip install jupyterlab_code_formatter
+jupyter labextension install @ryantam626/jupyterlab_code_formatter
+jupyter serverextension enable --py jupyterlab_code_formatter
+
+upyter labextension install jupyterlab_vim
+
+jupyter labextension install @jupyterlab/git
+pip install jupyterlab-git
+jupyter serverextension enable --py jupyterlab_git
+
+pip install ipywidgets
+jupyter labextension install @jupyter-widgets/jupyterlab-manager
+jupyter nbextension enable --py --sys-prefix widgetsnbextension
+
