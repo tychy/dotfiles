06f70d69a517848ddf7356477a0a7c73df0cb178
Ryu Yamada <a2sin2a2ko@kyoto-y.work>
2023-02-03 13:15:09 +0900

    attributes

--- link.sh
+++ link.sh
@@ -2,4 +2,5 @@ chsh -s $(which zsh)
 ln -s $HOME/dotfiles/.latexmkrc $HOME/.latexmkrc
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.gitconfig $HOME/.gitconfig
+ln -s $HOME/dotfiles/.gitattributes $HOME/.gitattributes
 # ln -s .gitconfig_work ~/.gitconfig_os
